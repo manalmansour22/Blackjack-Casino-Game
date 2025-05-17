cd461158e9df263bed66c4a3ad9e8e8be5ed2aee
manalmansour22 <manal.work91@hotmail.com>
2025-05-17 10:55:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let firstCard = 10;
+let secondCard = 11;
+let cards = [firstCard,secondCard];
+let sumCards = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+let messageEl=document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl= document.querySelector("#card-el");
+
+function startGame(){
+    renderGame();
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1] + " " + cards[2];
+    sumEl.textContent = "Sum: " + sumCards;
+    if(sumCards <= 20){
+        message= "Do you want to draw a new card?";
+    }else if(sumCards === 21){
+        message= "You've got Blackjack!";
+        hasBlackJack = true;
+    }else{
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = message ;
+}
+
+function newCard(){
+    let card = 8;
+    cards.push(card);
+    sumCards += card;
+    renderGame();
+}
+
+function count() {
+    let count;
+    for( count = 10 ;count < 21; count+=1)
+}
