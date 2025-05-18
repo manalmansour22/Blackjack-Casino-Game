fbb1e099831fa2379435c8a2ea7a8e5b584ab981
manalmansour22 <manal.work91@hotmail.com>
2025-05-18 09:59:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let firstCard = 10;
-let secondCard = 11;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard,secondCard];
 let sumCards = firstCard + secondCard;
 let hasBlackJack = false;
@@ -10,12 +10,21 @@ let messageEl=document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl= document.querySelector("#card-el");
 
+
+function getRandomCard(){
+    let randomCard = Math.floor(Math.random()*13)+1;
+    return randomCard;
+}
 function startGame(){
     renderGame();
 }
 
 function renderGame(){
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1] + " " + cards[2];
+    cardsEl.textContent = "Cards: ";
+    // + cards[0] + " " + cards[1] + " " + cards[2]
+    for(let i=0; i<cards.length; i++){
+        cardsEl.textContent+=cards[i]+" "
+    }
     sumEl.textContent = "Sum: " + sumCards;
     if(sumCards <= 20){
         message= "Do you want to draw a new card?";
@@ -30,10 +39,8 @@ function renderGame(){
 }
 
 function newCard(){
-    let card = 8;
+    let card = getRandomCard();
     cards.push(card);
     sumCards += card;
     renderGame();
 }
-
-
